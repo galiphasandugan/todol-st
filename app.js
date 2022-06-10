bb05cbfd49dbaa1555ced23ef005f4a43879498e
galiphasandugan <galiphasandugan@gmail.com>
2022-06-10 22:16:50 +0300

    first commit

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-const input = document.querySelector("#input");
-const buton = document.querySelector("i");
-const a = document.querySelector(".a");
-const sil = document.querySelector(".btn");
-const çiz = document.querySelector(".btn1");
+const ekle = document.querySelector(".btn");
+const çiz = document.querySelector(".a");
+const sil = document.querySelector(".b");
+const input = document.querySelector(".input");
+const div = document.createElement("div");
+const satır = document.querySelector(".main2");
+const main = document.querySelector(".main1");
+const a = document.querySelector("p");
 
-const ul = document.createElement("ul");
-const input1 = document.createElement("input");
 
-a.appendChild(ul);
+satır.appendChild(div);
 
-const b = ul.closest(".header").firstElementChild;
-b.style.width = "";
+// const b = ul.closest(".header").firstElementChild;
+// b.style.width = "";
 
-buton.onclick = function () {
+ekle.onclick = function () {
   if (!input.value) {
     alert("Lütfen bir bilgi giriniz");
   } else {
-    ul.innerHTML += ` <li>${input.value}</li>`;
-    input.value = "";
-  }
-  console.log(ul.childElementCount);
-};
+    div.innerHTML += `<div class="main1">
+          <button class="a"><i class="fa-solid fa-check"></i></button>
+          <p>${input.value}</p>
+          <button class="b"><i class="fa-solid fa-trash"></i></button>
+        </div>`;
+        input.value = "";
+        
+        
+      }
+      // console.log(ul.childElementCount);
+    };
+    
+    sil.onclick = function () {
+        a.innerHTML =  `remove()`
+    };
+    çiz.onclick = function () {
+        a.style.textDecoration = `line-through`
+  
+    };
+    // çiz.onclick = function () {
+//   ul.style.color = v`red`;
+// };
 
-sil.onclick = function () {
-  if (ul.childElementCount > 0) {
-    ul.removeChild(ul.lastElementChild);
-  } else {
-    alert("ailinecek dil kalmadı");
-  }
-};
-
-çiz.onclick = function () {
-  ul.style.color = v`red`;
-};
-
-input.addEventListener("keydown", (e) => {
-  // console.log(e);
-  if (e.keyCode === 13) {
-    buton.onclick();
-  }
-  // if (e.code === 'Enter') {
-  //   ekleBtn.onclick();
-  // }
-  if (e.code === "Delete") {
-    sil.onclick();
-  }
-});
+// input.addEventListener("keydown", (e) => {
+//   // console.log(e);
+//   if (e.keyCode === 13) {
+//     buton.onclick();
+//   }
+//   // if (e.code === 'Enter') {
+//   //   ekleBtn.onclick();
+//   // }
+//   if (e.code === "Delete") {
+//     sil.onclick();
+//   }
+// });
